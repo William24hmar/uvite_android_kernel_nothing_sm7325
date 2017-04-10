3e167973079b8dd667e7e48f2d11e9112db7c79d
Andi Kleen <ak@linux.intel.com>
2017-04-09 22:10:40 -0700

    lto: Use C version for SYSCALL_ALIAS / cond_syscall
    With LTO aliases get largely resolved in the compiler, not in the linker.
    
    Implement cond_syscall and SYSCALL_ALIAS in C to let the compiler
    understand the aliases so that it can resolve it properly.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>

--- include/linux/linkage.h
+++ include/linux/linkage.h
@@ -24,14 +24,12 @@
 
 #ifndef cond_syscall
 #define cond_syscall(x)	\
-	long __attribute__((weak, alias("sys_ni_syscall"))) x(void);
+	extern long x(void) __attribute__((alias("sys_ni_syscall"), weak));
 #endif
 
 #ifndef SYSCALL_ALIAS
-#define SYSCALL_ALIAS(alias, name) asm(			\
-	".globl " __stringify(alias) "\n\t"		\
-	".set   " __stringify(alias) ","		\
-		  __stringify(name))
+#define SYSCALL_ALIAS(a, name) \
+	long a(void) __attribute__((alias(__stringify(name))))
 #endif
 
 #define __page_aligned_data	__section(.data..page_aligned) __aligned(PAGE_SIZE)
