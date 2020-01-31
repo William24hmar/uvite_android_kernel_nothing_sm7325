49a6b37b145ea4fed4d89667082767f600b5778b
Qian Cai <cai@lca.pw>
2020-01-30 22:15:01 -0800

    mm/page_isolation: fix potential warning from user
    It makes sense to call the WARN_ON_ONCE(zone_idx(zone) == ZONE_MOVABLE)
    from start_isolate_page_range(), but should avoid triggering it from
    userspace, i.e, from is_mem_section_removable() because it could crash
    the system by a non-root user if warn_on_panic is set.
    
    While at it, simplify the code a bit by removing an unnecessary jump
    label.
    
    Link: http://lkml.kernel.org/r/20200120163915.1469-1-cai@lca.pw
    Signed-off-by: Qian Cai <cai@lca.pw>
    Suggested-by: Michal Hocko <mhocko@kernel.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8528,7 +8528,7 @@ struct page *has_unmovable_pages(struct zone *zone, struct page *page, int count
 		if (is_migrate_cma(migratetype))
 			return NULL;
 
-		goto unmovable;
+		return page;
 	}
 
 	for (found = 0; iter < pageblock_nr_pages; iter++) {
@@ -8540,7 +8540,7 @@ struct page *has_unmovable_pages(struct zone *zone, struct page *page, int count
 		page = pfn_to_page(check);
 
 		if (PageReserved(page))
-			goto unmovable;
+			return page;
 
 		/*
 		 * If the zone is movable and we have ruled out all reserved
@@ -8560,7 +8560,7 @@ struct page *has_unmovable_pages(struct zone *zone, struct page *page, int count
 			unsigned int skip_pages;
 
 			if (!hugepage_migration_supported(page_hstate(head)))
-				goto unmovable;
+				return page;
 
 			skip_pages = compound_nr(head) - (page - head);
 			iter += skip_pages - 1;
@@ -8605,12 +8605,9 @@ struct page *has_unmovable_pages(struct zone *zone, struct page *page, int count
 		 * page at boot.
 		 */
 		if (found > count)
-			goto unmovable;
+			return page;
 	}
 	return NULL;
-unmovable:
-	WARN_ON_ONCE(zone_idx(zone) == ZONE_MOVABLE);
-	return pfn_to_page(pfn + iter);
 }
 
 #ifdef CONFIG_CONTIG_ALLOC
