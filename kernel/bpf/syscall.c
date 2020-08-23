a4463337bc04ae71f20fce73302b1757df664bb4
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-08-23 17:36:59 -0500

    BACKPORT: treewide: Use fallthrough pseudo-keyword
    Replace the existing /* fall through */ comments and its variants with
    the new pseudo-keyword macro fallthrough[1]. Also, remove unnecessary
    fall-through markings when it is the case.
    
    Only apply changes to bpf, not treewide.
    
    [1] https://www.kernel.org/doc/html/v5.7/process/deprecated.html?highlight=fallthrough#implicit-switch-case-fall-through
    
    Change-Id: I56f83316fa51615b5e0bc37762f321223f7c1a7f
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2130,7 +2130,7 @@ bpf_prog_load_check_attach(enum bpf_prog_type prog_type,
 	case BPF_PROG_TYPE_EXT:
 		if (expected_attach_type)
 			return -EINVAL;
-		/* fallthrough */
+		fallthrough;
 	default:
 		return 0;
 	}
