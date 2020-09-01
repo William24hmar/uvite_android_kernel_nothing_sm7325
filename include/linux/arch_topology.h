1caaf1763517bdf7c618ae7d499081c0a5f2873e
Valentin Schneider <valentin.schneider@arm.com>
2020-09-01 21:55:48 +0100

    arch_topology, cpufreq: constify arch_* cpumasks
    The passed cpumask arguments to arch_set_freq_scale() and
    arch_freq_counters_available() are only iterated over, so reflect this
    in the prototype. This also allows to pass system cpumasks like
    cpu_online_mask without getting a warning.
    
    Change-Id: I495aceee428806d35e52ebccf0ad4354e7a2b6ca
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -59,7 +59,7 @@ void topology_set_thermal_pressure(const struct cpumask *cpus,
 void topology_update_thermal_pressure(const struct cpumask *cpus,
 				      unsigned long capped_freq);
 
-bool arch_freq_counters_available(struct cpumask *cpus);
+bool arch_freq_counters_available(const struct cpumask *cpus);
 
 struct cpu_topology {
 	int thread_id;
