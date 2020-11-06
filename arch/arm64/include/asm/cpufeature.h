6dcbce51748a88e37c2e3af8ec07081fea193a9e
Ionela Voinescu <ionela.voinescu@arm.com>
2020-11-06 12:53:32 +0000

    arm64: wrap and generalise counter read functions
    In preparation for other uses of Activity Monitors (AMU) cycle counters,
    place counter read functionality in generic functions that can reused:
    read_corecnt() and read_constcnt().
    
    As a result, implement update_freq_counters_refs() to replace
    init_cpu_freq_invariance_counters() and both initialise and update
    the per-cpu reference variables.
    
    Change-Id: I7e084585a5e62241713ce417792b3a3fc440eea7
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Link: https://lore.kernel.org/r/20201106125334.21570-2-ionela.voinescu@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -750,6 +750,16 @@ static inline bool cpu_has_hw_af(void)
 						ID_AA64MMFR1_HADBS_SHIFT);
 }
 
+#ifdef CONFIG_ARM64_AMU_EXTN
+/* Check whether the cpu supports the Activity Monitors Unit (AMU) */
+extern bool cpu_has_amu_feat(int cpu);
+#else
+static inline bool cpu_has_amu_feat(int cpu)
+{
+	return false;
+}
+#endif
+
 #endif /* __ASSEMBLY__ */
 
 #endif
