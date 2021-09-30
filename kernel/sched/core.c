7a7d8c78f39968c07a822a90970daa640ca6ae64
Xuewen Yan <xuewen.yan@unisoc.com>
2021-09-30 13:52:01 +0800

    sched: Introduce task-rotation feature
    [root cause  ] improve scheduler
    [changes     ] introduce task-rotation feature
    [side effects] performance
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:sharkl5pro
    [test case]:geekbench scheduler
    [reviewers   ] ke.wang vincent.wang
    [change_type ] feature
    [tag_product ] common
    
    In ARM Big.Little cpu topology, big task's number may be more than
    the number of cpu. In this case, some big tasks may only could run
    on the little core, it is not fair and performance maybe loss.
    
    Introduce rotation feature to improve the performance.
    
    At scheduler_tick, it would check the big core's task and small core's task,
    when the small's task is misfit, and the big core's task had running
    for a long time. It would swap the two task's cpu, so that the small core's
    task can run on big core.
    
    As a result, the big tasks can rotation running between little core and
    big core to improve the performance.
    
    Below is the test in Geekbench:
    
                    origin  modify
    muiti-core:     1460    1581(+8.3%)
    crypto:         2583    2691(+4.2%)
    integer:        1431    1560(+9.0%)
    floating:       1338    1443(+7.8%)
    
    Bug: 1727931
    Change-Id: Icfaafab7b3ea95b8143464bba8bdf83d82d312b8
    Signed-off-by: Xuewen Yan <xuewen.yan@unisoc.com>
    [dereference23: Kill check_for_migration()]
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    [Tashar02: raw_spin_unlock_irqrestore() -> raw_spin_rq_unlock_irqrestore()
    	   in load_balance()]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2048,6 +2048,9 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 		update_rq_clock(rq);
 
 	p->sched_class->enqueue_task(rq, p, flags);
+#ifdef CONFIG_SPRD_ROTATION_TASK
+	p->last_enqueue_ts = sched_ktime_clock();
+#endif
 	/*
 	 * Must be after ->enqueue_task() because ENQUEUE_DELAYED can clear
 	 * ->sched_delayed.
@@ -3075,7 +3078,7 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 	__set_task_cpu(p, new_cpu);
 }
 
-#ifdef CONFIG_NUMA_BALANCING
+#if defined(CONFIG_NUMA_BALANCING) || defined(CONFIG_SPRD_ROTATION_TASK)
 static void __migrate_swap_task(struct task_struct *p, int cpu)
 {
 	if (task_on_rq_queued(p)) {
@@ -3190,7 +3193,7 @@ int migrate_swap(struct task_struct *cur, struct task_struct *p,
 out:
 	return ret;
 }
-#endif /* CONFIG_NUMA_BALANCING */
+#endif /* CONFIG_NUMA_BALANCING || CONFIG_SPRD_ROTATION_TASK */
 
 /***
  * kick_process - kick a running thread to enter/exit the kernel
@@ -5165,6 +5168,10 @@ unsigned long long task_sched_runtime(struct task_struct *p)
 	return ns;
 }
 
+#ifdef CONFIG_SPRD_ROTATION_TASK
+static DEFINE_RAW_SPINLOCK(rotation_lock);
+#endif
+
 /*
  * This function gets called by the timer code, with HZ frequency.
  * We call it with interrupts disabled.
@@ -5195,6 +5202,16 @@ void scheduler_tick(void)
 #ifdef CONFIG_SMP
 	rq->idle_balance = idle_cpu(cpu);
 	trigger_load_balance(rq);
+#endif
+
+#ifdef CONFIG_SPRD_ROTATION_TASK
+	if (curr->sched_class == &fair_sched_class) {
+		if (rq->misfit_task_load && curr->state == TASK_RUNNING) {
+			raw_spin_lock(&rotation_lock);
+			check_for_task_rotation(rq);
+			raw_spin_unlock(&rotation_lock);
+		}
+	}
 #endif
 
 	trace_android_vh_scheduler_tick(rq);
