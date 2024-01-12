45ba9cdc5bf553cf5f37a7d35366ee5c2bea63de
Alexander Winkowski <dereference23@outlook.com>
2024-01-12 04:58:56 +0000

    Revert "ANDROID: mm/khugepaged: add missing vm_write_{begin|end}"
    This reverts commit 365a5b7af5fa6b3706aa8204772921172addc714.
    
    Change-Id: Ifb555936b0887bab47590ccd77bdd435da10c179
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1345,7 +1345,6 @@ void collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr)
 	if (!pmd)
 		goto drop_hpage;
 
-	vm_write_begin(vma);
 	/*
 	 * We need to lock the mapping so that from here on, only GUP-fast and
 	 * hardware page walks can access the parts of the page tables that
@@ -1413,7 +1412,7 @@ void collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr)
 				haddr + HPAGE_PMD_SIZE);
 	mmu_notifier_invalidate_range_start(&range);
 	_pmd = pmdp_collapse_flush(vma, haddr, pmd);
-	vm_write_end(vma);
+	spin_unlock(ptl);
 	mm_dec_nr_ptes(mm);
 	tlb_remove_table_sync_one();
 	mmu_notifier_invalidate_range_end(&range);
@@ -1430,7 +1429,6 @@ drop_hpage:
 
 abort:
 	pte_unmap_unlock(start_pte, ptl);
-	vm_write_end(vma);
 	i_mmap_unlock_write(vma->vm_file->f_mapping);
 	goto drop_hpage;
 }
@@ -1526,7 +1524,7 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 				mmu_notifier_invalidate_range_start(&range);
 				/* assume page table is clear */
 				_pmd = pmdp_collapse_flush(vma, addr, pmd);
-				vm_write_end(vma);
+				spin_unlock(ptl);
 				mm_dec_nr_ptes(mm);
 				tlb_remove_table_sync_one();
 				pte_free(mm, pmd_pgtable(_pmd));
