6b9b4e46fb7ab0955e1764e0e3be92a24d0ea079
TogoFire <togofire@mailfence.com>
2024-05-16 01:21:27 -0300

    EXFAT: Update exFAT driver
    (cherry picked from commit 86b8c7a6df84387a7f76793c81b8346bb0dabd41)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- fs/exfat/cache.c
+++ fs/exfat/cache.c
@@ -46,7 +46,11 @@ int exfat_cache_init(void)
 {
 	exfat_cachep = kmem_cache_create("exfat_cache",
 				sizeof(struct exfat_cache),
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 9, 0)
+				0, SLAB_RECLAIM_ACCOUNT,
+#else
 				0, SLAB_RECLAIM_ACCOUNT|SLAB_MEM_SPREAD,
+#endif
 				exfat_cache_init_once);
 	if (!exfat_cachep)
 		return -ENOMEM;
@@ -266,7 +270,7 @@ int exfat_get_cluster(struct inode *inode, unsigned int cluster,
 	struct exfat_inode_info *ei = EXFAT_I(inode);
 	struct buffer_head *bh = NULL;
 	struct exfat_cache_id cid;
-	unsigned int content, fclus;
+	unsigned int content = 0, fclus;
 	unsigned int end = cluster + *count - 1;
 
 	if (ei->start_clu == EXFAT_FREE_CLUSTER) {
