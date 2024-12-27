9a5f1ab512558f2a8325df0b5514d77e6c8185cd
Chengming Zhou <chengming.zhou@linux.dev>
2024-12-27 06:19:41 +0000

    BACKPORT: psi: Fix race when task wakes up before psi_sched_switch() adjusts flags
    When running hackbench in a cgroup with bandwidth throttling enabled,
    following PSI splat was observed:
    
        psi: inconsistent task state! task=1831:hackbench cpu=8 psi_flags=14 clear=0 set=4
    
    When investigating the series of events leading up to the splat,
    following sequence was observed:
    
        [008] d..2.: sched_switch: ... ==> next_comm=hackbench next_pid=1831 next_prio=120
            ...
        [008] dN.2.: dequeue_entity(task delayed): task=hackbench pid=1831 cfs_rq->throttled=0
        [008] dN.2.: pick_task_fair: check_cfs_rq_runtime() throttled cfs_rq on CPU8
        # CPU8 goes into newidle balance and releases the rq lock
            ...
        # CPU15 on same LLC Domain is trying to wakeup hackbench(pid=1831)
        [015] d..4.: psi_flags_change: psi: task state: task=1831:hackbench cpu=8 psi_flags=14 clear=0 set=4 final=14 # Splat (cfs_rq->throttled=1)
        [015] d..4.: sched_wakeup: comm=hackbench pid=1831 prio=120 target_cpu=008 # Task has woken on a throttled hierarchy
        [008] d..2.: sched_switch: prev_comm=hackbench prev_pid=1831 prev_prio=120 prev_state=S ==> ...
    
    psi_dequeue() relies on psi_sched_switch() to set the correct PSI flags
    for the blocked entity, however, with the introduction of DELAY_DEQUEUE,
    the block task can wakeup when newidle balance drops the runqueue lock
    during __schedule().
    
    If a task wakes before psi_sched_switch() adjusts the PSI flags, skip
    any modifications in psi_enqueue() which would still see the flags of a
    running task and not a blocked one. Instead, rely on psi_sched_switch()
    to do the right thing.
    
    Since the status returned by try_to_block_task() may no longer be true
    by the time schedule reaches psi_sched_switch(), check if the task is
    blocked or not using a combination of task_on_rq_queued() and
    p->se.sched_delayed checks.
    
    [ prateek: Commit message, testing, early bailout in psi_enqueue() ]
    
    Fixes: 152e11f6df29 ("sched/fair: Implement delayed dequeue") # 1a6151017ee5
    Signed-off-by: Chengming Zhou <chengming.zhou@linux.dev>
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Chengming Zhou <chengming.zhou@linux.dev>
    Link: https://lore.kernel.org/r/20241227061941.2315-1-kprateek.nayak@amd.com
    [Tashar02: Backport to msm-5.4]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6112,7 +6112,6 @@ pick_next_task(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
  */
 static void __sched notrace __schedule(int sched_mode)
 {
-	bool block = false;
 	struct task_struct *prev, *next;
 	bool preempt = sched_mode > SM_NONE;
 	unsigned long *switch_count;
@@ -6199,7 +6198,6 @@ static void __sched notrace __schedule(int sched_mode)
 			 * After this, schedule() must not care about p->state any more.
 			 */
 			block_task(rq, prev, flags);
-			block = true;
 		}
 		switch_count = &prev->nvcsw;
 	}
@@ -6233,7 +6231,8 @@ picked:
 		++*switch_count;
 
 		migrate_disable_switch(rq, prev);
-		psi_sched_switch(prev, next, block);
+		psi_sched_switch(prev, next, !task_on_rq_queued(prev) ||
+					     prev->se.sched_delayed);
 
 		trace_sched_switch(preempt, prev, next, prev_state);
 
