ccb2e0d1488052b5f6d0dad8b6d55693f69d5753
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-05-05 18:00:30 +0600

    Revert "BACKPORT: arch_topology, arm, arm64: define arch_scale_freq_invariant()"
    This reverts commit 23665eec46cc287b28e90f54e0d87784aa38895b.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -22,11 +22,6 @@
 #include <linux/smp.h>
 #include <trace/hooks/topology.h>
 
-bool topology_scale_freq_invariant(void)
-{
-	return cpufreq_supports_freq_invariance();
-}
-
 DEFINE_PER_CPU(unsigned long, freq_scale) = SCHED_CAPACITY_SCALE;
 DEFINE_PER_CPU(unsigned long, max_cpu_freq);
 DEFINE_PER_CPU(unsigned long, max_freq_scale) = SCHED_CAPACITY_SCALE;
