f754bed38b0a673724cc4c0531635150de1f99f2
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-05-24 02:24:58 +0600

    scripts/sorttable: Remove unused w2 and w8 function pointers
    Clang 23's new enhanced dead-store analysis correctly identifies that the w2 and w8
    static global function pointers are assigned in do_file() but never actually
    called, which causes the following warning:
    
    ../scripts/sorttable.c:57:15: warning: variable 'w2' set but not used [-Wunused-but-set-global]
       57 | static void (*w2)(uint16_t, uint16_t *);
          |               ^
    ../scripts/sorttable.c:58:15: warning: variable 'w8' set but not used [-Wunused-but-set-global]
       58 | static void (*w8)(uint64_t, uint64_t *);
          |               ^
    
    sorttable.h hardcodes the 32-bit 'w' writer to clear the main_extable_sort_needed
    flag (since sort_needed_loc is always a uint32_t *), rendering the 64-bit 'w8' and
    16-bit 'w2' writers entirely dead code.
    
    Remove them to fix the warning.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- scripts/sorttable.h
+++ scripts/sorttable.h
@@ -29,7 +29,6 @@
 #undef fn_ELF_R_INFO
 #undef uint_t
 #undef _r
-#undef _w
 
 #ifdef SORTTABLE_64
 # define extable_ent_size	16
@@ -51,7 +50,6 @@
 # define fn_ELF_R_INFO		fn_ELF64_R_INFO
 # define uint_t			uint64_t
 # define _r			r8
-# define _w			w8
 #else
 # define extable_ent_size	8
 # define compare_extable	compare_extable_32
@@ -72,7 +70,6 @@
 # define fn_ELF_R_INFO		fn_ELF32_R_INFO
 # define uint_t			uint32_t
 # define _r			r
-# define _w			w
 #endif
 
 static int compare_extable(const void *a, const void *b)
