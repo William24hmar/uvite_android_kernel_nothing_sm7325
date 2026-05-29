8520ced5d15b31b9568cbae85e8a2fc72fe7f2af
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-05-30 04:27:12 +0600

    [FIXUP] sched/fair: Implement delayed dequeue
    Reposition the migrating-task check in dequeue_entity() to evaluate
    task_on_rq_migrating() before the DELAY_DEQUEUE early-return path.
    
    This was not placed correctly due to an incorrect cherry-pick conflict resolution.
    
    No functional change.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5291,9 +5291,6 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	bool sleep = flags & DEQUEUE_SLEEP;
 	int action = UPDATE_TG;
 
-	if (entity_is_task(se) && task_on_rq_migrating(task_of(se)))
-		action |= DO_DETACH;
-
 	update_curr(cfs_rq);
 	clear_buddies(cfs_rq, se);
 
@@ -5318,6 +5315,9 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 			return false;
 		}
 	}
+
+	if (entity_is_task(se) && task_on_rq_migrating(task_of(se)))
+		action |= DO_DETACH;
 
 	/*
 	 * When dequeuing a sched_entity, we must:
